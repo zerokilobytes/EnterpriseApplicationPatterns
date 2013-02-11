f1ca4195dbb3470ff5a273b31c96bef0c5ecdd3a
zerokilobytes <markel.mairs@gmail.com>
2013-02-10 17:17:26 -0800

    delete domain logic example

--- src/org/eap/patterns/domainlogic/domainmodel/Product.java
+++
@@ -1,7 +0,0 @@
-package org.eap.patterns.domainlogic.domainmodel;
-
-public interface Product
-{
-	public String ProductName = null;
-	public RecognitionStrategy RecognitionStrategy = null;
-}
--- src/org/eap/patterns/domainlogic/domainmodel/RecognitionStrategy.java
+++
@@ -1,6 +0,0 @@
-package org.eap.patterns.domainlogic.domainmodel;
-
-public interface RecognitionStrategy 
-{
-	void CalculateRecognitions(Contract contract);
-}
